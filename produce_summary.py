3248ddf0a9221cd09deafc4d494e94d7d14fd10a
Sarah Patanroi <sarahpatanroi@gmail.com>
2021-07-23 23:12:46 +0000

    Added a dictionary for file <-> day data

--- produce_summary.py
+++ produce_summary.py
@@ -1,37 +1,37 @@
-def melon_delivery_report():
+def melon_delivery_report(file_name):
     '''Create a report of melons delivered'''
 
     #The 3 log files where data is stored
-    files = ("um-deliveries-20140519.txt","um-deliveries-20140519.txt","um-deliveries-20140519.txt")
+    files = {
+        "um-deliveries-20140519.txt": 1,
+        "um-deliveries-20140519.txt": 2,
+        "um-deliveries-20140519.txt": 3
+    }
+    #Print Day number as header 
+    print(f"\n\nDay {files[file_name]}\n---------")
+     
+    #Open file for the day
+    the_file = open(file_name)
 
-    #For every file index
-    for i in range(len(files)):
-
-        #Print Day number as header 
-        print(f"\n\nDay {i+1}\n---------")
-        
-        #Open file for the day
-        the_file = open(files[i])
-
-        #Read file, line by line
-        for line in the_file:
+    #Read file, line by line
+    for line in the_file:
             
-            #Strip any extra whitespace at end of line
-            line = line.rstrip()
+        #Strip any extra whitespace at end of line
+        line = line.rstrip()
 
-            #Split lines based on delimiter
-            words = line.split('|')
+        #Split lines based on delimiter
+        words = line.split('|')
 
-            #Capture line components into melon, count, amount
-            melon = words[0]
-            count = int(words[1])
-            amount = count * float(words[2])
+        #Capture line components into melon, count, amount
+        melon = words[0]
+        count = int(words[1])
+        amount = count * float(words[2])
 
-            #Print report
-            print(f"Delivered {count:5} {melon}s for total of ${amount:.2f}")
+        #Print report
+        print(f"Delivered {count:3} {melon}s for total of ${amount:.2f}")
 
-        #Close file
-        the_file.close()
+    #Close file
+    the_file.close()
 
 #Call above function
-melon_delivery_report()
+melon_delivery_report("um-deliveries-20140519.txt")
