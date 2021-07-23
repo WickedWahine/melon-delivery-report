ca44763eac46220caec15a00cd3f771e3361033c
Sarah Patanroi <sarahpatanroi@gmail.com>
2021-07-23 23:23:16 +0000

    Add check for valid argument

--- produce_summary.py
+++ produce_summary.py
@@ -4,31 +4,39 @@ def melon_delivery_report(file_name):
     #The 3 log files where data is stored
     files = {
         "um-deliveries-20140519.txt": 1,
-        "um-deliveries-20140519.txt": 2,
-        "um-deliveries-20140519.txt": 3
+        "um-deliveries-20140520.txt": 2,
+        "um-deliveries-20140521.txt": 3
     }
-    #Print Day number as header 
-    print(f"\n\nDay {files[file_name]}\n---------")
+
+    #Check that the file name argument is valid
+    if file_name not in files:
+        print("File does not exist, please check your input")
+        return False
+
+    else:
+
+        #Print Day number as header 
+        print(f"\n\nDAY {files[file_name]}\n---------")
      
-    #Open file for the day
-    the_file = open(file_name)
+        #Open file for the day
+        the_file = open(file_name)
 
-    #Read file, line by line
-    for line in the_file:
+        #Read file, line by line
+        for line in the_file:
             
-        #Strip any extra whitespace at end of line
-        line = line.rstrip()
+            #Strip any extra whitespace at end of line
+            line = line.rstrip()
 
-        #Split lines based on delimiter
-        words = line.split('|')
+            #Split lines based on delimiter
+            words = line.split('|')
 
-        #Capture line components into melon, count, amount
-        melon = words[0]
-        count = int(words[1])
-        amount = count * float(words[2])
+            #Capture line components into melon, count, amount
+            melon = words[0]
+            count = int(words[1])
+            amount = count * float(words[2])
 
-        #Print report
-        print(f"Delivered {count:3} {melon}s for total of ${amount:.2f}")
+            #Print report
+            print(f"Delivered {count:3} {melon}s for total of ${amount:.2f}")
 
     #Close file
     the_file.close()
